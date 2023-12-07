3411923d511bc6e4f7612998e7f079b596e7d39d
pemb6273 <PEMB6273@OX.AC.UK>
2023-12-07 20:46:30 +0000

    final answers for Q5

--- question-5-data/question5answers.R
+++ question-5-data/question5answers.R
@@ -1,19 +1,19 @@
 data <- Cui_etal2014
 
-# to transform V = βL^α into a linear model, we can log both sides to make it into log(V) = αlog(L) + log(β)
+# To transform V = βL^α into a linear model, we can log both sides to make it into log(V) = αlog(L) + log(β)
 
 log_V <- log(data$`Virion volume (nm×nm×nm)`)
 log_L <- log(data$`Genome length (kb)`)
 
-# we can now fit it to a linear model 
+# We can now fit it to a linear model 
 
 linear_model <- lm(log_V ~ log_L, data = data)
 
 summary(linear_model)
 
-# finding α and β
+# Finding α and β
 
-# log(β) is the intercept value 
+# Log(β) is the intercept value 
 
 exp(7.0748)
 
@@ -23,9 +23,9 @@ exp(7.0748)
 
 # α <- 1.5152
 
-# p value: 6.438e-10 < 0.05. We can say that the p value is statistically significant, as it's less than 0.05.
+# P value associated with intercept= 6.438e-10. P value associated with gradient = 2.28e-10. We can say that the gradient and intercept are statistically significant, as the p values for both are less than 0.05.
 
-# code for graph
+# Code for graph
 
 install.packages("ggplot2")
 install.packages("tidyverse")
@@ -33,10 +33,10 @@ library(ggplot2)
 library(tidyverse)
 
 data %>%
-ggplot(aes(x = log_L, y = log_V)) + geom_point() + geom_smooth(method = "lm") + labs(x = "log[Genome Length (kb)]", y= "log[Virion volume (nm3)]") + theme
+ggplot(aes(x = log_L, y = log_V)) + geom_point() + geom_smooth(method = "lm") + labs(x = "log[Genome Length (kb)]", y= "log[Virion volume (nm3)]") + theme_bw()
 
-# what is the estimated volume of a 300kb virus?
+# What is the estimated volume of a 300kb virus?
 
 1.512*log(300) + 7.0748
 
-# the estimated volume as worked out from inputting these numbers into the transformed formula equals 15.69892
+# The estimated volume as worked out from inputting these numbers into the transformed formula equals 15.69892
